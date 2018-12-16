1a2c4d94ce0c9fc7bb3aaccb9d0aa395b3c7e1bf
Abhishek Ravi <abhishekravi@abhisheks-macbook-pro.local>
2018-12-15 22:17:47 -0500

    Initial Commit

---
+++ StratoSnap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StratoSnap
+//
+//  Created by Abhishek  Ravi on 2018-12-15.
+//  Copyright © 2018 Abhishek  Ravi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ StratoSnapTests/StratoSnapTests.swift
@@ -0,0 +1,34 @@
+//
+//  StratoSnapTests.swift
+//  StratoSnapTests
+//
+//  Created by Abhishek  Ravi on 2018-12-15.
+//  Copyright © 2018 Abhishek  Ravi. All rights reserved.
+//
+
+import XCTest
+@testable import StratoSnap
+
+class StratoSnapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
